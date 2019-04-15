80ec3c0fefe8b05ea1360797800289b1830df755
agindt <agindt@css.edu>
2019-04-15 09:14:20 -0500

    Did what I could

--- app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
+++ app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+/**
+ * MainActivity sets up actions for the program
+ * @author      Adam Gindt
+ * @author      Tom Gibbons
+ * @version     1.0
+ */
 public class MainActivity extends AppCompatActivity {
     EditText etLoc;
     EditText etTemp;
@@ -21,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     AsyncDownloadXML AsyncWeatherDownloader = new AsyncDownloadXML();
 
     @Override
+    /**
+     * onCreate sets up actions when the main activity is created
+     * @param       Bundle
+     * @return      void
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -32,24 +43,48 @@ public class MainActivity extends AppCompatActivity {
         etVis =  (EditText) findViewById(R.id.textVis);
     }
 
+    /**
+     * btnClick sets up actions upon clicking the button
+     * @param   View
+     * @return  void
+     */
     public void btnClick (View v) throws XmlPullParserException, URISyntaxException, IOException {
         // Download the weather asynchronously
         AsyncWeatherDownloader.execute(this);
     }
 
+    /**
+     * Sets temperature value
+     * @param       temperature value in String format
+     * @return      void
+     */
     public void setTemp(String newTemp) {
         etTemp.setText(newTemp);
     }
 
+    /**
+     * Sets wind value
+     * @param       wind value in String format
+     * @return      void
+     */
     public void setWind(String newWind) {
         etWind.setText(newWind);
     }
 
+    /**
+     * Gets location value
+     * @return      String of location
+     */
     public String getLocation() {
         return etLoc.getText().toString();
     }
 
 
+    /**
+     * Sets the current status
+     * @param       status value in String format
+     * @return      void
+     */
     public void setStatus(String newStatus) {
         Toast toast=Toast.makeText(getApplicationContext(), newStatus,Toast.LENGTH_LONG );
         toast.show();
